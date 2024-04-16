f5efa23694c8462f802470e1aac66893d3a2204c
LutzBY <lutzby@gmail.com>
2024-04-16 10:50:07 +0300

    modified:   vlk_full_check_3.8.py

--- vlk_full_check_3.8.py
+++ vlk_full_check_3.8.py
@@ -1,94 +1,109 @@
-import requests
 from urllib.parse import urlencode
-import re
-import pandas as pd
-from bs4 import BeautifulSoup
-import time
-import datetime
-from datetime import datetime
-import json
-import datetime
-import os
-import requests
-from tqdm import tqdm
-from requests.exceptions import SSLError
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import psycopg2
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE
 
+# Чтение конфига с пасвордами
+with open('config.txt', 'r') as file:
+    lines = file.readlines()
+
+mail_login = lines[1].strip()
+mail_password = lines[3].strip()
+pgre_login = lines[5].strip()
+pgre_password = lines[7].strip()
+pgre_host = lines[9].strip()
+pgre_port = lines[11].strip()
+pgre_db = lines[13].strip()
+    
 #Подключение к postgres
 conn = psycopg2.connect(
-    host="root.alxsaz.net",
-    port = "443",
-    database="testdb",
-    user="user",
-    password="KuAxX6zP4ZCC4Kt"
+    host = pgre_host,
+    port = pgre_port,
+    database = pgre_db,
+    user = pgre_login,
+    password = pgre_password
 )
+# Отбираем строки с пустыми влк
+cursor = conn.cursor()
+query = """
+    SELECT brand, model, model_misc, year, cylinders, capacity, type, id, model_vlk
+    FROM av_full
+    where model_vlk is null
+    and brand = 'Honda';
+"""
+cursor.execute(query)
+row = cursor.fetchall()
+cursor.close()
 
-excel_file = "av_rq_full.xlsx"
-df_actual = pd.read_excel(excel_file)
-
-df = pd.DataFrame(columns=["!Model_VLK"])
-
-for index, row in tqdm(df_actual.iterrows(), total=len(df_actual)):
-    brand_row = row['Brand']
-    brand_row_lower = str(brand_row).lower()
-    year_row = row['Year']
-    displacement_row = int(row['Capacity'])
-    model_row = row['Model']
-    cyl_row = row['Cylinders']
-    mtype_row = row['Type']
-    model_misc = row['Model misc']
-    model_concat = model_row
-    if model_misc and pd.notnull(model_misc): # Если model_misc пуста
-        model_concat += " " + model_misc
-    
-    id_row = row['ID']
-    mvlk_row = row['!Model_VLK']
-
-    if pd.isna(mvlk_row):
-        cursor = conn.cursor()
-        query = """
-            SELECT model, mtype
-            FROM vlookup
-            WHERE brand = %(brand)s
-                AND year = %(year)s
-                AND cylinders = %(cyl)s
-                AND displacement_ccm BETWEEN %(min_displacement)s AND %(max_displacement)s
-        """
-        params = {
-            'brand': brand_row_lower,
-            'year': year_row,
-            'cyl': cyl_row,
-            'min_displacement': displacement_row - 53,
-            'max_displacement': displacement_row + 49
-        }
-        cursor.execute(query, params)
-        rows = cursor.fetchall()
-
-        match_ratio = []
-        best_match_list = []
-        best_match = ""
-        for row in rows:
-            model_found = row[0]
-            mtype_found = row[1]
-                
-            if mtype_found == mtype_row:
-                model_comp = fuzz.WRatio(model_concat.lower(), model_found.lower())
-                match_ratio.append(model_comp + 50)
-                best_match_list.append(model_found)
-            else:
-                model_comp = fuzz.WRatio(model_concat.lower(), model_found.lower())
-                match_ratio.append(model_comp)
-                best_match_list.append(model_found)
-            model_ratio_list = list(zip(best_match_list, match_ratio))
-        
-            best_model, best_ratio = max(model_ratio_list, key=lambda x: x[1])
-            best_match = best_model
-        df_actual.loc[index, "!Model_VLK"] = best_match
+# Функция дополнения модели инф-ой из базы знаний по постре
+for brand, model, modification, year, cylcount, capacity, mtype, id, mvlk in row:
+    if modification:
+        model_concat = model + " " + modification
     else:
-        continue
-    cursor.close()
-df_actual.to_excel(excel_file, index=False)
-conn.close()
+        model_concat = model
+    
+    displacement = float(capacity)
 
+    vlkcursor = conn.cursor()
+    query = """
+        SELECT model, mtype
+        FROM vlookup
+        WHERE brand = %(brand)s
+            AND year = %(year)s
+            AND cylinders = %(cyl)s
+            AND displacement_ccm BETWEEN %(min_displacement)s AND %(max_displacement)s
+    """
+    params = {
+        'brand': str(brand).lower(),
+        'year': year,
+        'cyl': cylcount,
+        'min_displacement': displacement - 53,
+        'max_displacement': displacement + 49
+    }
+    vlkcursor.execute(query, params)
+    rows = vlkcursor.fetchall()
+
+    match_ratio = []
+    best_match_list = []
+    best_match = ""
+    best_ratio = 0
+    for row in rows:
+        model_found = row[0]
+        mtype_found = row[1]
+        
+        if mtype_found == mtype:
+            model_comp = fuzz.WRatio(model_concat.lower(), model_found.lower())
+            match_ratio.append(model_comp + 30)
+            best_match_list.append(model_found)
+            print (f"row {row}, ratio - {model_comp + 30}")
+        else:
+            model_comp = fuzz.WRatio(model_concat.lower(), model_found.lower())
+            match_ratio.append(model_comp)
+            best_match_list.append(model_found)
+            print (f"row {row}, ratio - {model_comp}")
+        model_ratio_list = list(zip(best_match_list, match_ratio))
+        
+        best_model, best_ratio = max(model_ratio_list, key=lambda x: x[1])
+        best_match = best_model
+
+        print(F"MC - {model_concat}")
+        print (f"id - {id}, {brand} {model} {modification}, {year}, d - {capacity}, c - {cylcount}, t - {mtype},")
+        print(F"BM - {best_match}")
+        print(f"-----------")
+        
+        insertcursor = conn.cursor()
+        insertquery = """
+            UPDATE av_full
+            SET model_vlk = %s
+            WHERE id = %s
+        """
+        insertcursor.execute(insertquery, (best_match, id))
+        insertcursor.close()
+
+
+    vlkcursor.close()
+
+#conn.commit()
+conn.close()
