4919e73b0368d946607cb8eec55cdab835508517
LutzBY <lutzby@gmail.com>
2025-11-27 13:39:24 +0300

    modified:   av_parser_utils.py     modified:   avparser.py     Замена модели для vlk_llk т.к. старая возвращала ошибку 500

--- av_parser_utils.py
+++ av_parser_utils.py
@@ -81,7 +81,7 @@ def add_mvlk_llm (brand, model, modification, year, cylcount, capacity, mtype):
 
     # Создание чата и его атрибуты
     chat = client.chat.completions.create(
-        model= 'Llama-3.1-405B',
+        model= 'Llama-3.3-70B', #'Llama-3.1-405B' возвращает ошибку 500
         messages=messages,
         temperature=0,
         #max_tokens=64
--- avparser.py
+++ avparser.py
@@ -1,5 +1,5 @@
 # AVPARSER #
-version = '25.10.2025'
+version = '27.11.2025'
 
 import requests
 from urllib.parse import urlencode
@@ -251,7 +251,7 @@ def add_mvlk_llm (brand, model, modification, year, cylcount, capacity, mtype):
 
     # Создание чата и его атрибуты
     chat = client.chat.completions.create(
-        model="Llama-3.1-405B", # 100/1000 tokens, 39/message ## claude-3-haiku (10/Kt, 19/mess), Claude-Haiku-3.5 (30/1Kt, 42/mess) Llama-3.3-70B (130/mess), gpt-3.5-turbo (15/, 9/), GPT-4o-mini (5/1Kt, 5/mess)
+        model='Llama-3.3-70B', # #'Llama-3.1-405B' возвращает ошибку 500 100/1000 tokens, 39/message ## claude-3-haiku (10/Kt, 19/mess), Claude-Haiku-3.5 (30/1Kt, 42/mess) Llama-3.3-70B (130/mess), gpt-3.5-turbo (15/, 9/), GPT-4o-mini (5/1Kt, 5/mess)
         messages=messages,
         temperature=0,
         #max_tokens=64
